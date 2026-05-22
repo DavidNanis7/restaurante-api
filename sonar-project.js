956858dc57483666db679199527da7d0d82f9720
PL- David Zapata <dzapata@plinco.local>
2026-05-22 10:02:56 -0500

    fix: corregir sintaxis e importacion de sonarqube-scanner

--- sonar-project.js
+++ sonar-project.js
@@ -1,15 +1,16 @@
-// La variable debe llamarse 'scanner' (en inglés y con minúscula)
-const { scanner } = require('sonarqube-scanner');
+const sonarScanner = require('sonarqube-scanner').sonarScanner;
 
-scanner({
+sonarScanner({
   serverUrl: 'http://localhost:9000',
   options: {
     'sonar.projectKey': 'restaurante-api',
-    'sonar.projectName': 'API Sistema Restaurante',
+    'sonar.projectName': 'restaurante-api',
     'sonar.projectVersion': '1.0.0',
     'sonar.sources': 'src',
-    'sonar.tests': 'src/tests',
-    'sonar.javascript.environments': '["node", "jest"]',
-    'sonar.login': 'squ_ae5b1b2605dfc8b2887f0b8f8a6a8ce70e2926dd' // Pon aquí el token que generaste
+    'sonar.language': 'js',
+    'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+    'sonar.sourceEncoding': 'UTF-8'
   }
-}, () => {});
+}, () => {
+  console.log('Análisis estático con SonarQube finalizado.');
+});
