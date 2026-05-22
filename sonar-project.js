9fc855609caa39dfdb4a9c7ddbd97453481ba7dc
PL- David Zapata <dzapata@plinco.local>
2026-05-22 09:31:18 -0500

    chore: agregar configuracion e integracion de SonarQube en el pipeline

--- sonar-project.js
+++ sonar-project.js
@@ -1,19 +1,11 @@
-const sonarqubeScanner = require('sonarqube-scanner').default;
+const sonarqubeScanner = require('sonarqube-scanner');
 
-sonarqubeScanner(
-  {
-    serverUrl: 'http://localhost:9000',
-    options: {
-      'sonar.projectKey': 'restaurante-api',
-      'sonar.projectName': 'API Sistema Restaurante',
-      'sonar.projectVersion': '1.0.0',
-      'sonar.sources': 'src',
-      'sonar.tests': 'src/tests', // Si tu carpeta de tests está en otra ruta, cámbiala aquí
-      'sonar.language': 'js',
-      'sonar.sourceEncoding': 'UTF-8',
-      'sonar.exclusions': 'node_modules/**, src/tests/**',
-      'sonar.token': 'squ_ae5b1b2605dfc8b2887f0b8f8a6a8ce70e2926dd' 
-    }
-  },
-  () => process.exit()
-);
+sonarqubeScanner({
+  serverUrl: 'http://localhost:9000',
+  options: {
+    'sonar.projectKey': 'restaurante-api',
+    'sonar.projectName': 'API Sistema Restaurante',
+    // ¡Asegúrate de que esta línea use comillas dobles internas de esta manera!:
+    'sonar.javascript.environments': '["node", "jest"]'
+  }
+}, () => {});
