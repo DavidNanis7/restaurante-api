7f6f1ac5ee315e09598ef8133d881ce6ac32089f
PL- David Zapata <dzapata@plinco.local>
2026-05-22 09:45:47 -0500

    chore: agregar token de autenticacion para analisis de sonarqube

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,15 @@
-// Modifica la importación agregando las llaves { }
 const { scanner } = require('sonarqube-scanner');
 
-// Llama directamente a "scanner" en lugar de "sonarqubeScanner"
 scanner({
   serverUrl: 'http://localhost:9000',
   options: {
     'sonar.projectKey': 'restaurante-api',
     'sonar.projectName': 'API Sistema Restaurante',
+    'sonar.projectVersion': '1.0.0', // Añadimos la versión del proyecto
     'sonar.sources': 'src',
     'sonar.tests': 'src/tests',
-    'sonar.javascript.environments': '["node", "jest"]'
+    'sonar.javascript.environments': '["node", "jest"]',
+    // ¡AQUÍ PEGA TU TOKEN DE SONARQUBE!
+    'sonar.login': 'squ_ae5b1b2605dfc8b2887f0b8f8a6a8ce70e2926dd' 
   }
 }, () => {});
