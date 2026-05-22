d92d4c82b1af2c723fc15e5355f58f49acc02c27
PL- David Zapata <dzapata@plinco.local>
2026-05-22 09:35:30 -0500

    chore: agregar configuracion e integracion de SonarQube en el pipeline

--- sonar-project.js
+++ sonar-project.js
@@ -5,7 +5,9 @@ sonarqubeScanner({
   options: {
     'sonar.projectKey': 'restaurante-api',
     'sonar.projectName': 'API Sistema Restaurante',
-    // ¡Asegúrate de que esta línea use comillas dobles internas de esta manera!:
+    'sonar.sources': 'src',
+    'sonar.tests': 'src/tests',
+    // ¡AQUÍ ES DONDE VA LA LÍNEA QUE DIO EL ERROR ANTERIOR!
     'sonar.javascript.environments': '["node", "jest"]'
   }
 }, () => {});
