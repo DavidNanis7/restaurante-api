deef99a718b3c86b16edb84fa9dff77ef3ec11e8
PL- David Zapata <dzapata@plinco.local>
2026-05-22 09:49:52 -0500

    fix: corregir traduccion automatica de scanner en ingles

--- sonar-project.js
+++ sonar-project.js
@@ -1,3 +1,4 @@
+// La variable debe llamarse 'scanner' (en inglés y con minúscula)
 const { scanner } = require('sonarqube-scanner');
 
 scanner({
@@ -5,11 +6,10 @@ scanner({
   options: {
     'sonar.projectKey': 'restaurante-api',
     'sonar.projectName': 'API Sistema Restaurante',
-    'sonar.projectVersion': '1.0.0', // Añadimos la versión del proyecto
+    'sonar.projectVersion': '1.0.0',
     'sonar.sources': 'src',
     'sonar.tests': 'src/tests',
     'sonar.javascript.environments': '["node", "jest"]',
-    // ¡AQUÍ PEGA TU TOKEN DE SONARQUBE!
-    'sonar.login': 'squ_ae5b1b2605dfc8b2887f0b8f8a6a8ce70e2926dd' 
+    'sonar.login': 'squ_ae5b1b2605dfc8b2887f0b8f8a6a8ce70e2926dd' // Pon aquí el token que generaste
   }
 }, () => {});
